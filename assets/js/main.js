adb25ec2dfef234fdf3f9ab805f82f264178e462
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-12-06 17:06:43 -0300

    ADicionado funcao ao clicar enter

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,20 @@ function createTaskItem() {
 }
 
 function createTask(task) {
+  if (!task) return;
+
   const li = createTaskItem();
 
   li.innerText = task;
   tasks.appendChild(li);
 }
 
+inputTask.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13)
+    createTask(inputTask.value);
+});
+
 
 btnAddTask.addEventListener('click', function (e) {
-  if (!inputTask.value) return;
-
   createTask(inputTask.value);
 });
