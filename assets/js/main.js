4fe237b80f2ad761867e6043ce47d97a8db87fc9
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-12-06 17:04:52 -0300

    Add metodo de criar tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,22 @@ const inputTask = document.querySelector('.input-new-task');
 const btnAddTask = document.querySelector('.btn-add-task');
 const tasks = document.querySelector('.tasks');
 
-function createTask(task) {
-  console.log(task);
+function createTaskItem() {
+  const li = document.createElement('li');
+
+  return li;
 }
+
+function createTask(task) {
+  const li = createTaskItem();
+
+  li.innerText = task;
+  tasks.appendChild(li);
+}
+
 
 btnAddTask.addEventListener('click', function (e) {
   if (!inputTask.value) return;
 
   createTask(inputTask.value);
-
 });
